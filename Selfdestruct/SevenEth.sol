ea0fa78b5cfc0567c054969da94d7c7a277a5d69
10ap17 <150276227+10ap17@users.noreply.github.com>
2024-02-14 20:13:04 +0100

    Add files via upload

---
+++ Selfdestruct/SevenEth.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+contract SevenEth {
+    function play() external payable {
+        require(msg.value == 1 ether, "not 1 ether");
+
+        uint256 bal = address(this).balance;
+        require(bal <= 7 ether, "game over");
+
+        if (bal == 7 ether) {
+            payable(msg.sender).transfer(7 ether);
+        }
+    }
+}
